14efedeb2c8adb55cc3b2967d583736e14358ce9
matheusz98 <matheuscastiglia31@gmail.com>
2021-06-03 17:56:35 -0300

    Lembrete

--- Projeto Agenda de Compromissos/src/App.js
+++ Projeto Agenda de Compromissos/src/App.js
@@ -30,12 +30,17 @@ function App() {
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
   };
+
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder} : task))
+  };
+
     return (
       <div className="container">
         <div className="content">
           <Header />
           {tasks.length > 0 ? (
-            <Tasks tasks={tasks} onDelete={deleteTask} />
+            <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
           ) : (
             'Nenhum compromisso marcado.'
           )}
--- Projeto Agenda de Compromissos/src/components/Task.js
+++ Projeto Agenda de Compromissos/src/components/Task.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { FaTimes } from 'react-icons/fa';
 
-function Task({ task, onDelete }) {
+function Task({ task, onDelete, onToggle }) {
     return (
-        <div className="task">
+        <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}>
             <h3>{task.text} <FaTimes style={{color: 'red', cursor: 'pointer'}} onClick={() => onDelete(task.id)} /></h3>
             <p>{task.day}</p>
         </div>
