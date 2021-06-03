9d9ad75dbe9c66c675b4f506742126f1f37de66d
matheusz98 <matheuscastiglia31@gmail.com>
2021-06-03 15:17:12 -0300

    Header e Estilização

--- Projeto Agenda de Compromissos/src/App.js
+++ Projeto Agenda de Compromissos/src/App.js
@@ -3,8 +3,10 @@ import Header from './components/Header';
 
 function App() {
   return (
-    <div className="app">
-      <Header />
+    <div className="container">
+      <div className="content">
+        <Header />
+      </div>
     </div>
   );
 }
--- Projeto Agenda de Compromissos/src/components/Header.js
+++ Projeto Agenda de Compromissos/src/components/Header.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { FaPlus } from 'react-icons/fa';
 
-function Header() {
+function Header({ title }) {
     return (
-        <header>
-            <h2>Hello World!</h2>
+        <header className="header">
+            <h1 className="title">{title}</h1>
+            <button className="btn"><FaPlus /></button>
         </header>
     );
+}
+
+Header.defaultProps = {
+    title: 'Agenda de Compromissos',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
 }
 
 export default Header;
