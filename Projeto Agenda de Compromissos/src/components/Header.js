359963c96fc9c3917aefbe737711b3a89da3768b
matheusz98 <matheuscastiglia31@gmail.com>
2021-06-04 03:27:04 -0300

    Finalizando o projeto

--- Projeto Agenda de Compromissos/src/components/Header.js
+++ Projeto Agenda de Compromissos/src/components/Header.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from './Button';
 
 function Header({ title }) {
     return (
         <header className="header">
             <h1 className="title">{title}</h1>
-            <Button />
         </header>
     );
 }
